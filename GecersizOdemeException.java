2d0d6c82205c71e50815fba775088c72430d5e3e
yusufaydgn7 <ysf4471@gmail.com>
2026-05-06 16:36:40 +0300

    Delete GecersizOdemeException.java

--- GecersizOdemeException.java
+++
@@ -1,9 +0,0 @@
-package sporSalonuÜyelikVeAntrenmanProgramı;
-
-public class GecersizOdemeException extends Exception {
-    private static final long serialVersionUID = 1L;
-
-    public GecersizOdemeException(String mesaj) {
-        super(mesaj);
-    }
-}
