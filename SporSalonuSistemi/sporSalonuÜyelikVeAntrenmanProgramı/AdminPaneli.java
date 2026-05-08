8ee5ebc0c5c8392a7173da5c95290363c510bd80
Eyüp Sansar <eyupsnsr@gmail.com>
2026-05-08 21:58:59 +0300

    hatalr giderildi

--- SporSalonuSistemi/sporSalonuÜyelikVeAntrenmanProgramı/AdminPaneli.java
+++ SporSalonuSistemi/sporSalonuÜyelikVeAntrenmanProgramı/AdminPaneli.java
@@ -17,7 +17,7 @@ public class AdminPaneli extends JFrame {
         this.admin = admin;
 
         setTitle("Yönetici Paneli - " + admin.getIsim()+" "+admin.getSoyisim());
-        setSize(900, 600);
+        setSize(1200, 900);
         setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         setLocationRelativeTo(null);
         
